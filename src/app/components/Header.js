5b58348fd57fb343df718a36a3b43d5353da2520
Elpidio Armando <jun.r.armando@gmail.com>
2017-06-10 10:31:34 +0800

    initial router setup

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,14 +2,15 @@ import React from "react";
 
 export const Header = (props) => {
     return (
-        <div className="navbar navbar-default">
+        <nav className="navbar navbar-default">
             <div className="container">
                 <div className="navbar-header">
                     <ul className="nav navbar-nav">
-                        <li><a href="#">{props.homeLink}</a></li>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">User</a></li>
                     </ul>
                 </div>
             </div>
-        </div>           
+        </nav>           
     );
 }
